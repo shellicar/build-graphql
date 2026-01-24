0cc091b460ff4a11fc28b6292306a94664efd510
Stephen Hellicar <shellicar@gmail.com>
2026-01-24 15:17:36 +1100

    Tweak biome config.

--- packages/build-graphql/src/core/graphql/findGraphQLFiles.ts
+++ packages/build-graphql/src/core/graphql/findGraphQLFiles.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 import { findFiles } from '../findFiles';
 import type { ResolvedOptions } from '../types';
 import type { GraphqlFile } from './types';
